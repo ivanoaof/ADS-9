3d9579053c585cca2d62da50247bddbad8c0cdc0
ivanoaof <151541394+ivanoaof@users.noreply.github.com>
2025-06-07 19:38:44 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,30 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
+#include <memory>
 
+class PMTree {
+public:
+    struct Node {
+        char value;
+        std::vector<std::shared_ptr<Node>> children;
+        Node(char val) : value(val) {}
+    };
+
+    PMTree(const std::vector<char>& elements);
+    ~PMTree() = default;
+
+    std::shared_ptr<Node> getRoot() const { return root; }
+    size_t getPermutationsCount() const { return total_permutations; }
+
+private:
+    std::shared_ptr<Node> root;
+    size_t total_permutations;
+    void buildTree(std::shared_ptr<Node> parent, const std::vector<char>& remaining);
+};
+
+std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
+std::vector<char> getPerm1(const PMTree& tree, int num);
+std::vector<char> getPerm2(const PMTree& tree, int num);
 #endif  // INCLUDE_TREE_H_
