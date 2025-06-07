fe8c765a32de848f9e05301fb1df33763f6861a8
ivanoaof <151541394+ivanoaof@users.noreply.github.com>
2025-06-07 19:52:35 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -18,7 +18,8 @@ class PMTree {
  private:
     std::shared_ptr<Node> root;
     size_t total_permutations;
-    void buildTree(std::shared_ptr<Node> parent, const std::vector<char>& remaining);
+    void buildTree(std::shared_ptr<Node> parent, 
+const std::vector<char>& remaining);
 };
 std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
 std::vector<char> getPerm1(const PMTree& tree, int num);
